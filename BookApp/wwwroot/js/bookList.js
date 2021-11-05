70185fb6a56872f83b0db0b42b187fc87536eac5
Girish <girish@post.com>
2021-11-04 18:21:48 -0700

    initalize bookList

--- BookApp/wwwroot/js/bookList.js
+++ BookApp/wwwroot/js/bookList.js
@@ -1,10 +1,19 @@
 ﻿var BookList = (function ($) {
     // 
     var filterApiUrl = null;
+
+    function sendForm(inputElem) {
+        var form = $(inputElem).parents('form');
+        form.submit();
+    }
     return {
         initialise: function (filterByValue, filterValue, exFilterApiUrl) {
             filterApiUrl = exFilterApiUrl;
             console.log(filterByValue, filterValue,filterApiUrl);
+        },
+
+        sendForm: function (inputElem) {
+            sendForm(inputElem);
         }
     }
 }(window.jQuery))
